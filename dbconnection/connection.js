640a2613ab36aa8c6f067201251c34b97491cf62
sneha kawale <sneha.kawale@pinnacle.in>
2023-05-23 15:23:27 +0530

    'commit'

---
+++ dbconnection/connection.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+router.use(express.json());
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/user', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const userSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: String,
+    password: String,
+});
+
+const User = mongoose.model('user_registration', userSchema);
+
+module.exports = User;
